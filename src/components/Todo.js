2ec040487e6c38961b37f482437c23e941b050da
er.jyotirmoy.nath@gmail.com <anjana@123>
2018-01-20 17:08:08 +0530

    MobX Complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,35 @@
+@observer
 class Todo extends React.Component {
     constructor(props) {
         super(props);
     }
-    deleteTodo(){
-    	todoactions.removeTodo(this.props.id);
+    createTodo(e){
+      if(e.which == 13){
+        this.props.store.createnewTodo(e.target.value);
+
+      }
+    }
+    filter(e){
+      this.props.store.filter = e.target.value;
+    }
+    completeTodo(todo){
+        todo.completed = !todo.completed;
     }
     render() {
+
+      let {todos,filter,filteredtodos,removeComplted} = this.props.store;
+      let todoli = filteredtodos.map((item) => {
+        console.log(item.text);
+        return <li><input type="checkbox" value={item.completed} checked={item.completed} onChange={this.completeTodo.bind(this,item)} />{item.text}</li>
+      });
         return (
-            <li >{this.props.text}<button className="btn btn-default" onClick={this.deleteTodo.bind(this)}>X</button></li>
+          <div>
+
+          <input type="text" onKeyPress={this.createTodo.bind(this)} />
+          <input type="text" onChange={this.filter.bind(this)}  />
+            <ul>{todoli}</ul>
+            <button onClick={removeComplted} >Clear</button>
+          </div>
         );
     }
 }
--- src/store/Todostore.js
+++ src/store/Todostore.js
@@ -1,6 +1,28 @@
-class Todostore {
-	@observable todos = ['buy eggs','buy milk']
-	@observable filter = ''
+class Todoitem {
+	@observable text
+	@observable id
+	@observable completed
+	constructor(value){
+		this.text = value
+		this.id = Date.now()
+		this.completed = false
+		}
 }
-var store = new Todostore;
-console.log(store);
+class Todostore {
+	@observable todos = []
+	@observable filter = ''
+	@computed get filteredtodos (){
+		var matchfilter = new RegExp(this.filter,'i');
+		return this.todos.filter(item => !this.filter || matchfilter.test(item.text));
+	}
+
+	createnewTodo(value){
+		this.todos.push(new Todoitem(value));
+	}
+
+	removeComplted = ()=>{
+		const notcomplted = this.todos.filter(item =>  !item.completed);
+		this.todos.replace(notcomplted);
+	}
+}
+var store = window.store = new Todostore;
